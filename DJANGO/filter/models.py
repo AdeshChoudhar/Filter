459db950fbdab943e4ace6ebf1866c56e96c7f69
Adesh Choudhar <choudharas19.comp@coep.ac.in>
2021-04-09 20:19:13 +0530

    Finally Folderized Properly

--- DJANGO/filter/models.py
+++ DJANGO/filter/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
+    id = models.AutoField(primary_key=True)
     image = models.ImageField(upload_to='images/')
